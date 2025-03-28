8fe57e9637a3a816a3b488d987e2516649f85a14
Ricardo laiginhas <ricardo.laiginhas.lec_a@msft.cesae.pt>
2025-03-28 17:53:37 +0000

    Fix database to wait for MySQL before WordPress config

--- main.tf
+++ main.tf
@@ -55,31 +55,37 @@ resource "aws_instance" "wordpress" {
               systemctl enable mariadb
               systemctl start mariadb
 
-              # Criar base de dados e utilizador
+              until mysqladmin ping --silent; do
+                echo "Waiting for MySQL to start..."
+                sleep 2
+              done
+
+
+            
               mysql -e "CREATE DATABASE wp_landing_db DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
               mysql -e "CREATE USER 'wp_ricardo'@'localhost' IDENTIFIED BY 'W0rdPr3ssRic2024!';"
               mysql -e "GRANT ALL PRIVILEGES ON wp_landing_db.* TO 'wp_ricardo'@'localhost';"
               mysql -e "FLUSH PRIVILEGES;"
 
-              # Instalar WordPress
+            
               cd /var/www/html
               wget https://wordpress.org/latest.tar.gz
               tar -xzf latest.tar.gz
               cp -r wordpress/* .
               rm -rf wordpress latest.tar.gz
 
-              # Criar wp-config.php automaticamente
+             
               cp wp-config-sample.php wp-config.php
               sed -i "s/database_name_here/wp_landing_db/" wp-config.php
               sed -i "s/username_here/wp_ricardo/" wp-config.php
               sed -i "s/password_here/W0rdPr3ssRic2024!/" wp-config.php
               sed -i "s/localhost/localhost/" wp-config.php
 
-              # Corrigir permissões
+             
               chown -R apache:apache /var/www/html
               chmod -R 755 /var/www/html
 
-              # Instalar e configurar CloudWatch Agent
+             
               yum install -y amazon-cloudwatch-agent
               cat > /opt/aws/amazon-cloudwatch-agent/bin/config.json << CONFIG
               {
